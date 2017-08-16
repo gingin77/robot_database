8ef70f3dc4d274a343635a5c458398b6a59a13eb
ginniehench <ginniehench@gmail.com>
2017-08-16 16:45:15 -0400

    most of the data is being pulled, some styling still needs to be done

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-
-// const path = require('path');
-
 const app = express();
 const dataset = require('./robot_data.js');
-
-const position = dataset.users[3].id;
-const url = position - 1;
-console.log(url);
-
+let index = "";
 
 
 
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', './views');
-
 
 // app.use('/robot_database', express.static('../robot_database'));
 app.use(express.static(__dirname + '/public'));
@@ -25,13 +17,10 @@ app.get('/index/', function (req,res){
   res.render('robot', dataset)
   })
 
-app.get('/index/url', function (req,res){
-    res.render('individual', dataset.users[position])
+app.get('/index/:one', function (req,res){
+    index = req.params.one-1;
+    res.render('individual', dataset.users[index])
     })
-
-// app.get('/index/', function (req,res){
-//   res.render('robot', dataset)
-//   })
 
 app.listen(3000, function () {
   console.log('Successfully started express application!');
