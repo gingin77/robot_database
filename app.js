5a040eee3bed547a9724fff2bbe85045ee4c69c5
ginniehench <ginniehench@gmail.com>
2017-08-16 15:35:27 -0400

    individual page loads a name related to a position variable but I need to switch to a dynamic route

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const mustacheExpress = require('mustache-express');
 const app = express();
 const dataset = require('./robot_data.js');
 
+const position = dataset.users[3].id;
+const url = position - 1;
+console.log(url);
+
 
 
 
@@ -17,9 +21,17 @@ app.set('views', './views');
 // app.use('/robot_database', express.static('../robot_database'));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/database/index', function (req,res){
+app.get('/index/', function (req,res){
   res.render('robot', dataset)
   })
+
+app.get('/index/url', function (req,res){
+    res.render('individual', dataset.users[position])
+    })
+
+// app.get('/index/', function (req,res){
+//   res.render('robot', dataset)
+//   })
 
 app.listen(3000, function () {
   console.log('Successfully started express application!');
