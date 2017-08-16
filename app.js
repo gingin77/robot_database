19e3ab1525bfd5c5532830ab7e86bd6fad61b757
ginniehench <ginniehench@gmail.com>
2017-08-16 13:39:47 -0400

    index page is rendering

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+
+// const path = require('path');
+
+const app = express();
+const dataset = require('./robot_data.js');
+
+
+
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', './views');
+
+
+// app.use('/robot_database', express.static('../robot_database'));
+
+app.get('/database/index', function (req,res){
+  res.render('robot', dataset)
+  })
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+})
